e2357b7c57ec0838114cc71baa40ea61c5624c9a
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-18 21:36:27 +0500

    3-masala

--- number_even.py
+++ number_even.py
@@ -1,4 +1,9 @@
 #A four-digit integer is given. Find the number of even digits in it.
 #Create a variable "var_int" and assign it a four-digit integer value.
-print(1)
+number = 8369
+bir = number//1000
+ikki = (number//100)%10
+uch = (number//10)%10
+turt = (number)%10
 #Print the number of even digits in the variable "var_int".
+print((bir-1)%2+(ikki-1)%2+(uch-1)%2+(turt-1)%2)
