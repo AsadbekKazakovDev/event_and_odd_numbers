5262baea10a6177aae5ae3b406bf568cfe3656a5
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-18 21:39:09 +0500

    3-masala

--- number_even.py
+++ number_even.py
@@ -6,4 +6,5 @@ ikki = (number//100)%10
 uch = (number//10)%10
 turt = (number)%10
 #Print the number of even digits in the variable "var_int".
-print((bir-1)%2+(ikki-1)%2+(uch-1)%2+(turt-1)%2)
+var_int = ((bir-1)%2+(ikki-1)%2+(uch-1)%2+(turt-1)%2)
+print(var_int)
