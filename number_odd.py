348b19081c8fd073779ce57883a556a4ed98992e
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-18 21:41:43 +0500

    4-masala

--- number_odd.py
+++ number_odd.py
@@ -1,4 +1,10 @@
 #A four-digit integer is given. Find the number of odd digits in it.
 #Create a variable "var_int" and assign it a four-digit integer value.
-print(1)
+number = 8375
+bir = number//1000
+ikki = (number//100)%10
+uch = (number//10)%10
+turt = (number)%10
 #Print the number of odd digits in the variable "var_int".
+var_int = ((bir)%2+(ikki)%2+(uch)%2+(turt)%2)
+print(var_int)
